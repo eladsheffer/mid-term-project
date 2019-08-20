6dbb8b28727bfce4de1b06bffdc591c7f7e22c2c
Elad <mastik8@gmail.com>
2019-08-20 09:36:22 +0300

    improvements

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var images = [];
-var i;
-images[0] = "../images/250X110668568400.jpg";
-images[1] = "../images/banner_atar.jpg";
-images[2] = "../images/paz_250x110103131473.jpg";
-images[3] = "../images/paz_250x110059890678.jpg";
+var i = 0;
+images[0] = "images/250X110668568400.jpg";
+images[1] = "images/banner_atar.jpg";
+images[2] = "images/paz_250x110103131473.jpg";
+images[3] = "images/paz_250x110059890678.jpg";
+
+function displayImg() {
+    if (i >= images.length - 1) { i = 0; }
+    document.getElementById("img").src = images[i];
+    i++;
+}
 
 function changeImg() {
-    for (i = 0; i < images.length; i++) {
-        setInterval(document.getElementById("img").src = images[i], 5000);
-    }
+    setInterval(displayImg(), 1000);
 }
